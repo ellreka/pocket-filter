e85caf865bc4cc1f64d4aff023cd86027c70835e
ellreka <kozakana333@gmail.com>
2019-10-23 18:11:34 +0900

    Add getAllTags

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,33 @@
 import request from 'request';
 
-const getArticles = (consumer_key: string, access_token: string, params: {}, callback: (body: {}) => void) => {
-    const options: { headers: {}; body: string; url: string } = {
-        headers: {
-            'content-type': 'application/x-www-form-urlencoded',
-            'X-Accept': 'application/json',
-        },
-        body: `consumer_key=${consumer_key}&access_token=${access_token}&detailType=complete&count=2`,
-        url: 'https://getpocket.com/v3/get',
-    };
+class PocketFilter {
+    consumerKey: string;
+    accessToken: string;
 
-    request.post(options, (error, response, body) => {
-        const pocket_data = JSON.parse(body)['list'];
-        const pocket_arr = Object.entries(pocket_data).map((val: any) => {
-            return {
-                item_id: val[1]['item_id'],
-                title: val[1]['resolved_title'],
-                url: val[1]['resolved_url'],
-                tags: Object.keys(val[1]['tags'] || { _untagged_: undefined }),
-                image: val[1]['image'] || { image: undefined },
-                time_added: val[1]['time_added'],
-                favorite: val[1]['favorite'],
-                status: val[1]['status'],
-            };
+    constructor(consumerKey: string, accessToken: string) {
+        this.consumerKey = consumerKey;
+        this.accessToken = accessToken;
+    }
+
+    getAllTags(callback: (body: {}) => void) {
+        const params = {
+            consumer_key: this.consumerKey,
+            access_token: this.accessToken,
+            detailType: 'complete',
+        };
+        request.post({ url: 'https://getpocket.com/v3/get', qs: params }, (error, response, body) => {
+            const pocket_data = JSON.parse(body)['list'];
+            const all_tag = Object.keys(pocket_data).flatMap(value => {
+                return pocket_data[value].tags ? Object.keys(pocket_data[value].tags) : undefined;
+            });
+            const tags_list = Array.from(new Set(all_tag)).filter(Boolean);
+            callback(tags_list);
         });
-        callback(pocket_arr);
-    });
-};
+    }
 
-const pocketfilter = {
-    getArticles: getArticles,
-};
+    getArticles() {
+        // request.post()
+    }
+}
 
-module.exports = pocketfilter;
+export default PocketFilter;
